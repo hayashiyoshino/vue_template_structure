aba157b0a45159a5b6a486c15d885bb90361239f
林佳志乃 <twinklestaryoshino@gmail.com>
2019-01-31 14:40:22 +0900

    try filter arguments

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-Vue.filter('numberFormat', function(value){
-  return value.toLocaleString()
+Vue.filter('readMore', function(text, length, suffix){
+  return text.substring(0, length) + suffix
 })
 
 var app = new Vue({
   el: '#app',
   data: {
-    price: 2980000000
-  }
-})
-
-var appp = new Vue({
-  el: '#appp',
-  data: {
-    price: 2980000000
+    text: 'まだあげ初めし前髪の林檎のもとに見えしとき前にさしたる花櫛の花ある君と思ひけりやさしく白き手をのべて林檎をわれにあたへしは薄紅の秋の実に人こひ初めしはじめなりわがこゝろなきためいきのその髪の毛にかゝるときたのしき恋の盃を君が情に酌みしかな林檎畑の樹の下におのづからなる細道は誰が踏みそめしかたみぞと問ひたまふこそこひしけれ'
   }
 })
